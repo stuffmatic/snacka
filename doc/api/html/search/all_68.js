ebc7ee8c736ff6d3935e8e70088ad8151f7a1d73
per <per@stuffmatic.com>
2013-07-30 16:13:24 +0200

    updated docs, cleaned up socket code, new autobahn test results

--- doc/api/html/search/all_68.js
+++ doc/api/html/search/all_68.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['header',['header',['../structsn_frame.html#affebaf36aa9dbf803fe8c43c279f2661',1,'snFrame']]],
   ['host',['host',['../structsn_websocket.html#aa410463f6d3a2294055e5ca0c9060468',1,'snWebsocket']]],
   ['httprequestpath',['httpRequestPath',['../structsn_websocket.html#afe9aa4d72d646ef86e433f77d7065f71',1,'snWebsocket']]]
 ];
--- doc/api/html/search/all_70.js
+++ doc/api/html/search/all_70.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['payload',['payload',['../structsn_frame.html#a30730df70becd292625bcf777b427c9f',1,'snFrame']]],
   ['payloadsize',['payloadSize',['../structsn_frame_header.html#a86d451c5d05ede4b06a444ec1de80cea',1,'snFrameHeader']]],
   ['port',['port',['../structsn_websocket.html#aaa4806a335890ff84721c3abe26a6e13',1,'snWebsocket']]]
 ];
